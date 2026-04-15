d56562b0d20e35248480b866e1612eb84a7caf5c
youarenotalone <165114717+RWKmoon@users.noreply.github.com>
2026-04-15 10:41:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// efeito do titulo (todas as paginas)
+const titulos = ["You", "are", "not", "alone."];
+let i = 0;
+
+setInterval(() => {
+  document.title = titulos[i];
+  i = (i + 1) % titulos.length;
+}, 300);
+
+// SISTEMA DE RESPOSTA (só roda se tiver pergunta)
+const pergunta = document.querySelector("h1");
+
+if (pergunta && pergunta.innerText.includes("5+3-2")) {
+  
+  // cria input escondido
+  const input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "...";
+  
+  input.style.background = "black";
+  input.style.color = "white";
+  input.style.border = "1px solid white";
+  input.style.marginTop = "20px";
+  input.style.textAlign = "center";
+
+  document.body.appendChild(input);
+
+  input.focus();
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      
+      const resposta = input.value.trim();
+
+      if (resposta === "6") {
+        window.location.href = "/enigma/6/";
+      } else {
+        input.value = "";
+        input.placeholder = "errado...";
+      }
+
+    }
+  });
+}
