00d98bd274201213f43685220aa856f91ea8bb93
youarenotalone <165114717+youarenotalone404@users.noreply.github.com>
2026-04-15 19:52:47 -0300

    Delete +app directory

--- +app/lastnumber.py
+++
@@ -1,7 +0,0 @@
-data = input("Que dia da semana é hoje? ").lower().strip()
-FimDaSemana = ("sabado", "domingo")
-
-if data in FimDaSemana:
-    print("Bom fim de semana")
-else:
-    print("Boa semana")
